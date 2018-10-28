2a8b6586f7d7c5194f894f08503913f629a445cc
junkai <junkai@qq.com>
2018-10-28 20:21:16 +0800

    jiaaaaaaa

---
+++ src/main/java/com/itheima/dao/Testee.java
@@ -0,0 +1,14 @@
+package com.itheima.dao;
+
+import org.junit.Test;
+
+public class Testee {
+
+    @Test
+   public void fun(){
+        System.out.println("wwww");
+        System.out.println("dddd");
+        System.out.println("xxxx");
+
+    }
+}
